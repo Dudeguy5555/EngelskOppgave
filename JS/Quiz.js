6ac2a0d323c902064b36ab4cc2c13ef45c1dca13
Aleksander Krakk <aleksanderhairball@gmail.com>
2026-01-15 11:11:38 +0100

    Startet quiz oppgave

---
+++ JS/Quiz.js
@@ -0,0 +1,27 @@
+const quetion = [
+  {
+    question: "Do we all",
+    options: ["Answer a", "Answer b", "Answer c"],
+    correct: 0,
+  },
+  {
+    question: "learn defeat",
+    options: ["Answer A", "Answer B", "Answer C"],
+    correct: 1,
+  },
+  {
+    question: "from the whores",
+    options: ["Answer A", "Answer B", "Answer C"],
+    correct: 2,
+  },
+  {
+    question: "with bad feet?",
+    options: ["Answer A", "Answer B", "Answer C"],
+    correct: 3,
+  },
+  {
+    question: "Beat the meat",
+    options: ["Answer A", "Answer B", "Answer C"],
+    correct: 4,
+  }
+];
